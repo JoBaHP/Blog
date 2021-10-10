826a51953646a583914b33db23bd5cc589c74faf
JoBaHP <jocoraos@yahoo.com>
2021-10-10 13:38:38 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("backend is running ...");
+});
